f5b74535a123b8e499a4a1bf4464be001f4dccbe
liray-unendlich <unenlichkeit629@gmail.com>
2022-09-29 13:44:34 +0900

    Add initialization of worker of phase0

---
+++ init-phase0-worker.sh
@@ -0,0 +1,39 @@
+#!bin/bash
+echo -n "Enter password:"
+read PERMISSION_PASSWORD
+
+echo "make directories: for postgres"
+mkdir -p data/postgres
+
+echo "port configuring"
+echo "$PERMISSION_PASSWORD" | sudo -S ufw allow 2377
+echo "$PERMISSION_PASSWORD" | sudo -S ufw allow 4789
+echo "$PERMISSION_PASSWORD" | sudo -S ufw allow 7946
+echo "$PERMISSION_PASSWORD" | sudo -S ufw reload
+
+source .env
+
+echo "change hostname to indexer-${CHAIN_1_NAME}"
+hostname indexer-${CHAIN_1_NAME}
+
+echo "restart docker"
+echo "$PERMISSION_PASSWORD" | sudo -S service docker restart
+sleep 10s
+
+echo -n "enter existing docker swarm token:"
+read DOCKER_SWARM_TOKEN
+echo -n "Enter existing docker swarm manager IP:"
+read DOCKER_SWARM_MANAGER_IP
+
+docker swarm join --token $DOCKER_SWARM_TOKEN $DOCKER_SWARM_MANAGER_IP
+
+echo "Initialization completed"
+
+echo "deploy ${CHAIN_1_NAME} indexer"
+set -o allexport; source .env; set +o allexport; envsubst < graphnode-config/config.tmpl > graphnode-config/config-${CHAIN_1_NAME}.toml
+(echo -e "version: '3.8'"; docker compose -f graph.yml --env-file .env config) > deployment/indexer-${CHAIN_1_NAME}.yml
+
+sed -i -e '2d' deployment/indexer-${CHAIN_1_NAME}.yml
+docker stack deploy -c deployment/indexer-${CHAIN_1_NAME}.yml indexer-${CHAIN_1_NAME}
+
+echo "Finished deployment"
