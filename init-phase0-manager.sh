1621ac9023a983fd8da57521b9ae7089cabff554
liray-unendlich <unenlichkeit629@gmail.com>
2022-09-29 16:30:40 +0900

    Simplify

--- init-phase0-manager.sh
+++ init-phase0-manager.sh
@@ -33,22 +33,18 @@ TRAEFIK_HASHED_PASSWORD=$(openssl passwd -apr1 $TRAEFIK_PASSWORD)
 PROMETHEUS_HASHED_PASSWORD=$(openssl passwd -apr1 $PROMETHEUS_PASSWORD)
 TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD//\$/\$\$}
 PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD//\$/\$\$}
-sed -i -e "$ a TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD}" .env
 sed -i -e "$ a PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD}" .env
 
-(TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD}; echo -e "version: '3.8'"; docker compose -f traefik.yml --env-file .env config) > deployment/traefik.yml
-(PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD}; echo -e "version: '3.8'"; docker compose -f swarmpit.yml --env-file .env config) > deployment/swarmpit.yml
-sed -i -e '2d' deployment/traefik.yml
-sed -i -e '2d' deployment/swarmpit.yml
-sed -i -e "$ d" .env
-sed -i -e "$ d" .env
+set -o allexport; source .env; TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD}; set +o allexport; envsubst < traefik.yml > deployment/traefik.yml
+(echo -e "version: '3.8'"; docker compose -f swarmpit.yml --env-file .env config) > deployment/swarmpit.yml
 
 echo "deploy traefik/swarmpit"
 docker stack deploy -c deployment/traefik.yml traefik
 docker stack deploy -c deployment/swarmpit.yml swarmpit
 
 echo "deploy monitor service"
-(echo -e "version: '3.8'"; docker compose -f monitor.yml --env-file .env config) > deployment/monitor.yml
+(PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD}; echo -e "version: '3.8'"; docker compose -f monitor.yml --env-file .env config) > deployment/monitor.yml
+sed -i -e "$ d" .env
 sed -i -e '2d' deployment/monitor.yml
 docker stack deploy -c deployment/monitor.yml monitor
 
