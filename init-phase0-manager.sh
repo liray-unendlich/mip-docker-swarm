921737eb78850dd7974712d876dba60e432ac33e
liray-unendlich <unenlichkeit629@gmail.com>
2022-09-29 13:27:32 +0900

    Fixed manager initialization: tested

--- init-phase0-manager.sh
+++ init-phase0-manager.sh
@@ -1,10 +1,11 @@
-#!bin/sh
+#!bin/bash
 echo -n "Enter password:"
 read PERMISSION_PASSWORD
 
 echo "make directories: for traefik/swarmpit/prometheus/grafana/postgres"
-mkdir -p .data/postgres .data/swarmpit/db-data .data/swarmpit/influx-data .data/traefik
-mkdir -p .data/prometheus .data/grafana
+mkdir -p data/postgres data/swarmpit/db-data data/swarmpit/influx-data data/traefik
+mkdir -p deployment
+mkdir -p data/prometheus data/grafana
 
 echo "port configuring"
 echo "$PERMISSION_PASSWORD" | sudo -S ufw allow 2377
@@ -13,7 +14,7 @@ echo "$PERMISSION_PASSWORD" | sudo -S ufw allow 7946
 echo "$PERMISSION_PASSWORD" | sudo -S ufw reload
 
 echo "change hostname to indexer-manager"
-hostname indexer-manager
+echo "$PERMISSION_PASSWORD" | sudo -S hostname indexer-manager
 
 echo "initialize docker swarm"
 docker swarm init
@@ -30,11 +31,17 @@ docker node update --label-add swarmpit.influx-data=true $NODE_ID
 source .env
 TRAEFIK_HASHED_PASSWORD=$(openssl passwd -apr1 $TRAEFIK_PASSWORD)
 PROMETHEUS_HASHED_PASSWORD=$(openssl passwd -apr1 $PROMETHEUS_PASSWORD)
+TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD//\$/\$\$}
+PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD//\$/\$\$}
+sed -i -e "$ a TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD}" .env
+sed -i -e "$ a PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD}" .env
 
-(echo -e "version: '3.8'"; docker compose -f traefik.yml --env-file .env config) > deployment/traefik.yml
-(echo -e "version: '3.8'"; docker compose -f swarmpit.yml --env-file .env config) > deployment/swarmpit.yml
+(TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD}; echo -e "version: '3.8'"; docker compose -f traefik.yml --env-file .env config) > deployment/traefik.yml
+(PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD}; echo -e "version: '3.8'"; docker compose -f swarmpit.yml --env-file .env config) > deployment/swarmpit.yml
 sed -i -e '2d' deployment/traefik.yml
 sed -i -e '2d' deployment/swarmpit.yml
+sed -i -e "$d" .env
+sed -i -e "$d" .env
 
 echo "deploy traefik/swarmpit"
 docker stack deploy -c deployment/traefik.yml traefik
