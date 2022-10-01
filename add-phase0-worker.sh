0c92bc9dda996b0d71055d9418fae5a5dff91d48
liray-unendlich <unenlichkeit629@gmail.com>
2022-10-02 02:54:07 +0900

    Fix typo

--- add-phase0-worker.sh
+++ add-phase0-worker.sh
@@ -13,7 +13,7 @@ docker config create config-${CHAIN_NAME}-$(date +"%Y%m%d") graphnode-config/con
 CHAIN_CONF_NAME=config-${CHAIN_NAME}-$(date +"%Y%m%d")
 
 echo "generate docker swarm configuration files(deployment/indexer-${CHAIN_NAME}.yml)"
-set -o allexport; source .env; CHAIN_0_NAME=$CHAIN_NAME; CHAIN_0_RPC=$CHAIN_RPC; CHAIN_CONF_NAME=${CHAIN_CONF_NAME} set +o allexport; envsubst < graph-node.tmpl.yml > deployment/indexer-${CHAIN_NAME}.yml
+set -o allexport; source .env; CHAIN_0_NAME=$CHAIN_NAME; CHAIN_0_RPC=$CHAIN_RPC; CHAIN_CONF_NAME=${CHAIN_CONF_NAME}; set +o allexport; envsubst < graph-node.tmpl.yml > deployment/indexer-${CHAIN_NAME}.yml
 #(echo -e "version: '3.8'"; CHAIN_0_NAME=$CHAIN_NAME; CHAIN_0_RPC=$CHAIN_RPC; docker compose -f graph-node.tmpl.yml --env-file .env config) > deployment/indexer-${CHAIN_NAME}.yml
 #sed -i -e '2d' deployment/indexer-${CHAIN_NAME}.yml
 
