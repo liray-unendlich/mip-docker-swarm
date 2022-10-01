508212560c92213974656026ba42ef6d94036f64
liray-unendlich <unenlichkeit629@gmail.com>
2022-10-01 12:57:38 +0900

    Change config usage

--- add-phase0-worker.sh
+++ add-phase0-worker.sh
@@ -9,10 +9,11 @@ echo "Entered chain RPC URL is: ${CHAIN_RPC}"
 
 echo "deploy configuration files"
 set -o allexport; source .env; CHAIN_0_NAME=$CHAIN_NAME; CHAIN_0_RPC=$CHAIN_RPC; set +o allexport; envsubst < graphnode-config/config.tmpl > graphnode-config/config-${CHAIN_NAME}.toml
-docker config create --label chain=${CHAIN_NAME} --label rev=$(date +"%Y%m%d") config-${CHAIN_NAME}.toml graphnode-config/config-${CHAIN_NAME}.toml
+docker config create config-${CHAIN_NAME}-$(date +"%Y%m%d") graphnode-config/config-${CHAIN_NAME}.toml
+CHAIN_CONF_NAME=config-${CHAIN_NAME}-$(date +"%Y%m%d")
 
 echo "generate docker swarm configuration files(deployment/indexer-${CHAIN_NAME}.yml)"
-set -o allexport; source .env; CHAIN_0_NAME=$CHAIN_NAME; CHAIN_0_RPC=$CHAIN_RPC; set +o allexport; envsubst < graph-node.tmpl.yml > deployment/indexer-${CHAIN_NAME}.yml
+set -o allexport; source .env; CHAIN_0_NAME=$CHAIN_NAME; CHAIN_0_RPC=$CHAIN_RPC; CHAIN_CONF_NAME=${CHAIN_CONF_NAME} set +o allexport; envsubst < graph-node.tmpl.yml > deployment/indexer-${CHAIN_NAME}.yml
 #(echo -e "version: '3.8'"; CHAIN_0_NAME=$CHAIN_NAME; CHAIN_0_RPC=$CHAIN_RPC; docker compose -f graph-node.tmpl.yml --env-file .env config) > deployment/indexer-${CHAIN_NAME}.yml
 #sed -i -e '2d' deployment/indexer-${CHAIN_NAME}.yml
 
