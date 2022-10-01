3cdb2a8aca7151b553ff6b7cb521faca0bc220b0
liray-unendlich <unenlichkeit629@gmail.com>
2022-10-01 12:54:13 +0900

    Add promtheus to use docker config

--- init-phase0-manager.sh
+++ init-phase0-manager.sh
@@ -34,6 +34,9 @@ PROMETHEUS_HASHED_PASSWORD=$(openssl passwd -apr1 $PROMETHEUS_PASSWORD)
 TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD//\$/\$\$}
 PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD//\$/\$\$}
 
+docker config create prometheus-$(date +"%Y%m%d") prometheus/prometheus.yml
+PROMETHEUS_CONF_NAME=prometheus-$(date +"%Y%m%d")
+
 set -o allexport; source .env; TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD}; set +o allexport; envsubst < traefik.yml > deployment/traefik.yml
 (echo -e "version: '3.8'"; docker compose -f swarmpit.yml --env-file .env config) > deployment/swarmpit.yml
 sed -i -e '2d' deployment/swarmpit.yml
@@ -43,7 +46,7 @@ docker stack deploy -c deployment/traefik.yml traefik
 docker stack deploy -c deployment/swarmpit.yml swarmpit
 
 echo "deploy monitor service"
-set -o allexport; source .env; PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD}; set +o allexport; envsubst < monitor.yml > deployment/monitor.yml
+set -o allexport; source .env; PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD}; PROMETHEUS_CONF_NAME=${PROMETHEUS_CONF_NAME}; set +o allexport; envsubst < monitor.yml > deployment/monitor.yml
 docker stack deploy -c deployment/monitor.yml monitor
 
 # show swarm token
