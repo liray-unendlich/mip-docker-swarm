075d821e37c657f66f4f58e716f58bb8fd8b8a91
liray-unendlich <unenlichkeit629@gmail.com>
2022-12-08 01:25:09 +0900

    Fix initializing script

--- init-manager.sh
+++ init-manager.sh
@@ -1,55 +1,87 @@
 #!bin/bash
-echo -n "Enter password:"
+echo "Starting initialization script of your indexer. Our setup procedure includes SSO authentication system."
+echo -n "Please enter your sudo password:"
 read PERMISSION_PASSWORD
 
-echo "make directories: for traefik/swarmpit/prometheus/grafana/postgres"
-mkdir -p data/postgres data/swarmpit/db-data data/swarmpit/influx-data data/traefik
+echo "Setting up directories under data/: for prometheus/grafana/postgres/authelia"
+mkdir -p data/postgres data/authelia/postgres data/authelia/config
 mkdir -p deployment
 mkdir -p data/prometheus data/grafana
 
-echo "port configuring"
+echo "Configuring ports"
 echo "$PERMISSION_PASSWORD" | sudo -S ufw allow 2377
 echo "$PERMISSION_PASSWORD" | sudo -S ufw allow 4789
 echo "$PERMISSION_PASSWORD" | sudo -S ufw allow 7946
 echo "$PERMISSION_PASSWORD" | sudo -S ufw reload
 
-echo "initialize docker swarm"
+echo "Initializing docker swarm"
 docker swarm init
 
-echo "create overlay network: traefik/monitor"
+echo "Creating overlay network: traefik/monitor"
 docker network create --driver=overlay traefik-public
 docker network create --driver=overlay monitor
 
-echo "generate docker swarm configuration files(deployment/*.yml)"
+echo "Generating docker swarm configuration files(deployment/*.yml)"
 NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 docker node update --label-add traefik-public.traefik-public-certificates=true $NODE_ID
-docker node update --label-add swarmpit.db-data=true $NODE_ID
-docker node update --label-add swarmpit.influx-data=true $NODE_ID
 source .env
-TRAEFIK_HASHED_PASSWORD=$(openssl passwd -apr1 $TRAEFIK_PASSWORD)
-PROMETHEUS_HASHED_PASSWORD=$(openssl passwd -apr1 $PROMETHEUS_PASSWORD)
-TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD//\$/\$\$}
-PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD//\$/\$\$}
 
 docker config create prometheus-$(date +"%Y%m%d") prometheus/prometheus.yml
 PROMETHEUS_CONF_NAME=prometheus-$(date +"%Y%m%d")
 
-set -o allexport; source .env; TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD}; set +o allexport; envsubst < ./template/traefik.tmpl.yml > deployment/traefik.yml
-(echo -e "version: '3.8'"; docker compose -f ./template/swarmpit.tmpl.yml --env-file .env config) > deployment/swarmpit.yml
-sed -i -e '2d' deployment/swarmpit.yml
+set -o allexport; source .env; set +o allexport; envsubst < ./template/authelia.tmpl.yml > deployment/authelia.yml
+set -o allexport; source .env; set +o allexport; envsubst < ./template/monitor.tmpl.yml > deployment/monitor.yml
+set -o allexport; source .env; set +o allexport; envsubst < ./template/portainer.tmpl.yml > deployment/portainer.yml
+set -o allexport; source .env; set +o allexport; envsubst < ./template/traefik.tmpl.yml > deployment/traefik.yml
 
-echo "deploy traefik/swarmpit"
+echo "Generating authelia secret files and deploying on docker swarm network"
+# Set up temporaly folder 'secrets' in deployment/
+mkdir deployment/secrets
+# Generate JWT_SECRET
+openssl rand -hex 64 > deployment/secrets/jwt_secret
+docker secret create jwt_secret deployment/secrets/jwt_secret
+# Generate SESSION_SECRET
+openssl rand -hex 64 > deployment/secrets/session_secret
+docker secret create session_secret deployment/secrets/session_secret
+# Generate STORAGE_POSTGRES_PASSWORD
+openssl rand -hex 64 > deployment/secrets/storage_postgres_password
+docker secret create storage_postgres_password deployment/secrets/storage_postgres_password
+# Generate STORAGE_ENCRYPTION_KEY
+openssl rand -hex 64 > deployment/secrets/storage_encryption_key
+docker secret create storage_encryption_key deployment/secrets/storage_encryption_key
+# Generate OIDC_HMAC_SECRET
+openssl rand -hex 64 > deployment/secrets/oidc_hmac_secret
+docker secret create oidc_hmac_secret deployment/secrets/oidc_hmac_secret
+# Generate OIDC_ISSUER_PRIVATE_KEY
+openssl genrsa -out deployment/secrets/oidc_issuer_private_key 4096
+docker secret create oidc_issuer_private_key deployment/secrets/oidc_issuer_private_key
+# Generate portainer client secret
+PORTAINER_CLIENT_SECRET=$(openssl rand -hex 64)
+
+# Delete secrets/*
+rm -r deployment/secrets
+
+# Generate configuration.yml
+set -o allexport; source .env; PORTAINER_CLIENT_SECRET=${PORTAINER_CLIENT_SECRET}; set +o allexport; envsubst < ./authelia/configuration.tmpl.yml > data/authelia/config/configuration.yml
+
+# Generate password hash and set up users_database.yml
+TEMP_HASHED_PASSWORD=$(docker run authelia/authelia:latest authelia crypto hash generate argon2 --password ${AUTHELIA_PASSWORD})
+AUTHELIA_HASHED_PASSWORD=${TEMP_HASHED_PASSWORD#Digest: }
+set -o allexport; source .env; AUTHELIA_HASHED_PASSWORD=${AUTHELIA_HASHED_PASSWORD}; set +o allexport; envsubst < ./authelia/users_database.tmpl.yml > data/authelia/config/users_database.yml
+
+echo "deploy traefik/authelia/portainer"
 docker stack deploy -c deployment/traefik.yml traefik
-docker stack deploy -c deployment/swarmpit.yml swarmpit
+docker stack deploy -c deployment/authelia.yml authelia
+docker stack deploy -c deployment/portainer.yml portainer
 
-echo "deploy monitor service"
-set -o allexport; source .env; PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD}; PROMETHEUS_CONF_NAME=${PROMETHEUS_CONF_NAME}; set +o allexport; envsubst < ./template/monitor.tmpl.yml > deployment/monitor.yml
+echo "deploy monitor stack"
+set -o allexport; source .env; set +o allexport; envsubst < ./template/monitor.tmpl.yml > deployment/monitor.yml
 docker stack deploy -c deployment/monitor.yml monitor
 
-# show swarm token
-echo "Copy below token for initializing new worker."
-echo "You can show this token again with: docker swarm join-token worker"
+# Generate and show portainer's oauth client secret
+echo -n "Please copy below secret string for editing your oauth configuration in portainer:"
+echo $PORTAINER_CLIENT_SECRET
 
-docker swarm join-token worker
+echo "If you forget portainer's client secret, use this command: cat data/authelia/config/configuration.yml and check bottom section"
 
 echo "Finished initializing manager node(indexer-manager)"
