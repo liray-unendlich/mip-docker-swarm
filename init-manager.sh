3a6ab94666860704a3216d48231b49468ac390c9
liray-unendlich <unenlichkeit629@gmail.com>
2023-01-10 23:07:02 +0900

    Fix stack deployment script
    - Remove initialize worker script
    - Edit init manager/update manager script
    - Add update indexer script

--- init-manager.sh
+++ init-manager.sh
@@ -39,10 +39,12 @@ PROMETHEUS_CONF_NAME=prometheus-$(date +"%Y%m%d")
 
 set -o allexport; source .env; TRAEFIK_HASHED_PASSWORD=${TRAEFIK_HASHED_PASSWORD}; set +o allexport; envsubst < ./template/traefik.tmpl.yml > deployment/traefik.yml
 set -o allexport; source .env; set +o allexport; envsubst < ./template/swarmpit.tmpl.yml > deployment/swarmpit.yml
+set -o allexport; source .env; set +o allexport; envsubst < ./template/portainer.tmpl.yml > deployment/portainer.yml
 
-echo "deploy traefik/swarmpit"
+echo "deploy traefik/swarmpit/portainer"
 docker stack deploy -c deployment/traefik.yml traefik
 docker stack deploy -c deployment/swarmpit.yml swarmpit
+docker stack deploy -c deployment/portainer.yml portainer
 
 echo "deploy monitor service"
 set -o allexport; source .env; PROMETHEUS_HASHED_PASSWORD=${PROMETHEUS_HASHED_PASSWORD}; PROMETHEUS_CONF_NAME=${PROMETHEUS_CONF_NAME}; set +o allexport; envsubst < ./template/monitor.tmpl.yml > deployment/monitor.yml
---
+++ update-indexer.sh
@@ -0,0 +1,63 @@
+#!bin/bash
+
+echo "pull latest code"
+/usr/bin/git pull
+
+echo "read .env"
+source .env
+
+echo "Did you completed to edit/update and rename your graph-node configuration file under graph-node-config/config.tmpl to graph-node-config/config.toml? Y/N:"
+
+if [ -z "$INDEXER_RPC" ]; then
+  echo -n "Enter mainnet RPC API(Full Node) URL:"
+  read INDEXER_RPC
+  sed -i -e "$ a INDEXER_RPC=${INDEXER_RPC}" .env
+fi
+
+if [ -z "$INDEXER_ADDRESS" ]; then
+  echo -n "Enter indexer address on mainnet:"
+  read INDEXER_ADDRESS
+  sed -i -e "$ a INDEXER_ADDRESS=${INDEXER_ADDRESS}" .env
+fi
+
+if [ -z "$INDEXER_MNEMONIC" ]; then
+  echo -n "Enter indexer mnemonic on mainnet:"
+  read INDEXER_MNEMONIC
+  sed -i -e "$ a INDEXER_MNEMONIC=\"${INDEXER_MNEMONIC}\"" .env
+fi
+
+if [ -z "$INDEXER_GEO" ]; then
+  echo -n "Enter your server location in LONGITUDE LATITUDE: like xx.xx yy.yy:"
+  read INDEXER_GEO
+  sed -i -e "$ a INDEXER_GEO=\"${INDEXER_GEO}\"" .env
+fi
+
+if [ -z "$INDEXER_DOMAIN" ]; then
+  echo -n "Enter your indexer domain:"
+  read INDEXER_DOMAIN
+  sed -i -e "$ a INDEXER_DOMAIN=${INDEXER_DOMAIN}" .env
+fi
+
+echo "Deploying configuration files"
+docker config create config-$(date +"%Y%m%d") graph-node-config/config.toml
+CHAIN_CONF_NAME=config-$(date +"%Y%m%d")
+
+echo "Generating docker swarm configuration files(deployment/indexer.yml)"
+set -o allexport; source .env; CHAIN_CONF_NAME=${CHAIN_CONF_NAME}; set +o allexport; envsubst < ./template/graph-node.tmpl.yml > deployment/graph-node.yml
+cp ./template/default.tmpl.agora > deployment/default.agora
+
+echo "Deploying/updating graph-node stack"
+docker stack deploy -c deployment/graph-node.yml graph-node
+
+# Wait 15 seconds to complete deploying and generate new database with postgres
+sleep 15s
+docker exec -e PGPASSWORD=${DB_PASSWORD} -it $(docker ps | grep postgres | grep graph-node |cut -c 1-12) psql -U ${DB_USER} -d ${DB_NAME} -c "CREATE DATABASE indexer;"
+
+echo "Deploying/updating indexer stack"
+set -o allexport; source .env; set +o allexport; envsubst < ./template/indexer.tmpl.yml > deployment/indexer.yml
+docker stack deploy -c deployment/indexer.yml indexer
+
+# Wait 15 seconds and try to connect indexer-agent
+sleep 15s
+docker exec -it $(docker ps | grep indexer-cli | cut -c 1-12) graph indexer connect http://indexer-agent:8000
+echo "indexer-service stack was deployed"
